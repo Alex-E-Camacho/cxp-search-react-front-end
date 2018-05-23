5073eac7a2835a55ba26a7b46567226da479a3c5
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-23 14:28:32 -0700

    Add constructor and state to App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      results: [],
+      selectedResults: []
+    }
+  }
+
+  
   render() {
     return (
       <div className="App">
-        
+
       </div>
     );
   }
