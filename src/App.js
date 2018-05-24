e877e519c8e99934b329d39023aadee6077c3748
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-24 09:28:32 -0700

    Add <div> around SearchBar

--- src/App.js
+++ src/App.js
@@ -29,7 +29,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SearchBar bingSearch={this.bingSearch} />
+        <div>
+          <SearchBar bingSearch={this.bingSearch} />
+        </div>
       </div>
     );
   }
