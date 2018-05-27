5f5757fd0bd937be59a001b8447534078b6d1989
Alex Camacho <alex.e.camacho@gmail.com>
2018-05-27 16:55:08 -0700

    Remove import of Component, remove stateful code from SearchBar component

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,34 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// class SearchBar extends Component {
-//   constructor(props) {
-//       super(props);
-//       this.state = {searchQuery: ''}
-
-//       this.handleChange = this.handleChange.bind(this);
-//       this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-
-//   handleChange(event) {
-//       this.setState({searchQuery: event.target.value});
-//   }
-
-//   handleSubmit(event) {
-//       this.props.bingSearch(this.state.searchQuery);
-//       event.preventDefault();
-//   }
-
-//     render() {
-//         return (
-//             <div>
-//                 <form onSubmit={this.handleSubmit}>
-//                     <input value={this.state.searchQuery} onChange={this.handleChange}/>
-//                     <input type="submit" value="Search" />
-//                 </form>
-//             </div>
-//         )
-//     }
-// }
 const SearchBar = (props) => {
 
    const handleSubmit = (event) => {
@@ -45,4 +16,5 @@ const SearchBar = (props) => {
         </div>
     )
 }
+
 export default SearchBar;
